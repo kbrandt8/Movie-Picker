c07f7ee8f92c7d5d9c369c7ef5e57e698ee84077
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2026-03-23 20:51:46 -0500

    Cookie Error
    cookieStore needed an await

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,32 @@
 import { getFavorites, getUserAccount } from '@/utils/getUser'
 import { cookies } from 'next/headers'
-import {getFaves} from '@/utils/getDash'
+import { getFaves } from '@/utils/getDash'
 import Dashboard from '@/components/dashboard/dashboard';
 import Movie from '@/components/dashboard/movie';
 import { redirect } from 'next/navigation'
 import MovieRecs from '@/components/dashboard/movie';
-export default async  function Page() {
-  const cookieStore = cookies()
-  
-    const session = cookieStore.get('movieSession')?.value || ""
-    const data = await getUserAccount(session)
-    const account_id = await data.id
-    const faves = await getFaves(account_id)
-    
-    if(session !== ""){
-  return (
-    <main>
-    <Dashboard data={data} />
-{faves.map((item:any)=>
-<div key={item.id}>
-  <MovieRecs movie={item} />
-  </div>
-)
-  }
-    </main>
+export default async function Page() {
+  const cookieStore = await cookies()
 
-  )}else{
+  const session = cookieStore.get('movieSession')?.value || ""
+  const data = await getUserAccount(session)
+  const account_id = await data.id
+  const faves = await getFaves(account_id)
+
+  if (session !== "") {
+    return (
+      <main>
+        <Dashboard data={data} />
+        {faves.map((item: any) =>
+          <div key={item.id}>
+            <MovieRecs movie={item} />
+          </div>
+        )
+        }
+      </main>
+
+    )
+  } else {
     redirect("/")
 
   }
